2793759b72fa77dea5cfde86e9f59d2d93d6dee8
Michael Lopes <michaellopes@Mac-mini-de-Michael.local>
2023-07-20 19:16:39 -0400

    Ajusts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.0-beta.3
+
+* Atualização de documentação.
+
 ## 1.0.0-beta.2
 
 * Atualização de documentação.
--- example/lib/business/repositories/thecocktaildb_repo.dart
+++ example/lib/business/repositories/thecocktaildb_repo.dart
@@ -6,6 +6,9 @@ abstract interface class ITheCocktailDBRepo {
 
 final class TheCocktailDBRepo extends FkBaseRepository
     implements ITheCocktailDBRepo {
+
+TheCocktailDBRepo() :super(baseUrl: "<BASE URL EXCLUSIVA>")
+  
   @override
   Future<FkHttpDriverResponse> searchDrinks(String term) {
     //BASE URL IS SETTED ON main.dart
